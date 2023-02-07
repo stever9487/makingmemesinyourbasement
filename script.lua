833d7b43d6a07fc244d6f2df9e1bde0ea0365242
stever9487 <50070094+stever9487@users.noreply.github.com>
2023-02-07 15:04:21 +0100

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,11 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt"))()
+local window = library:Window("making memes in your basement at 3 AM tycoon gui")
+local server = win:Server("main", "")
+local main = server:Channel("main")
+local tycoon
+for i, v in pairs(game:GetService("Workspace").Tycoons:GetChildren()) do
+    if v:FindFirstChild("TycoonOwner").Value == game:GetService("Players").LocalPlayer.Name then
+        tycoon = v
+        main:Label("tycoon: "..v.Name)
+    end
+end
