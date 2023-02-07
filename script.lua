40fd68b71dc164deb7d67d38fcde18ef839f3ede
stever9487 <50070094+stever9487@users.noreply.github.com>
2023-02-07 15:12:42 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,6 +1,6 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt"))()
 local window = library:Window("making memes in your basement at 3 AM tycoon gui")
-local server = win:Server("main", "")
+local server = window:Server("main", "")
 local main = server:Channel("main")
 local tycoon
 for i, v in pairs(game:GetService("Workspace").Tycoons:GetChildren()) do
